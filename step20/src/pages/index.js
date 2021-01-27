b39f161ee777b2b451b3e38351d88e0dd23e8ff0
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-01-27 20:01:00 +0500

    Step20 (gatsby_netlify_functions_faunadb)
    1:- gatsby new step18 https://github.com/gatsbyjs/gatsby-starter-hello-world
    2:- create netlify.toml file
    3:- "netlify functions:create hello"
    4:- "npm init"
    5:- "npm i --save faunadb"
    6:- update hello.js
    7:- create .dnv file
    8:- update index.js file

---
+++ step20/src/pages/index.js
@@ -0,0 +1,27 @@
+import React, {useState, useEffect} from "react"
+
+export default function Home() {
+  const [mydata, setData] = useState("Default Hello");
+
+  useEffect(() => {
+    console.log("useEffect Called");
+    fetch(`/.netlify/functions/hello`)
+      .then(response => response.json())
+      .then(data => {
+        setData(data);
+        console.log("Data: " + JSON.stringify(data));
+        
+      });
+  }, []);
+
+  console.log(mydata)
+
+
+
+
+
+  return <div>
+    <div>Hello world with Gatsby from Netlify!</div>
+    <div>{mydata.message}</div>
+    </div>
+}
